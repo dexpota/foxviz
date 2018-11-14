91845037c46c6ad61356f7a8aa567d756bcd4e84
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-11-14 20:24:40 +0100

    First commit.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main(Main.class, args);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+
+        line(5, 5, 50, 50);
+    }
+}
