5dd3ba0b9afbbbad017a3066e24ea22e195dab37
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-11-15 22:41:18 +0100

    Modified the project and added a dependency. argparse4j to parse the command line.

--- src/main/java/me/destro/foxviz/Main.java
+++ src/main/java/me/destro/foxviz/Main.java
@@ -1,10 +1,31 @@
 package me.destro.foxviz;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 import processing.core.PApplet;
 
 public class Main extends PApplet {
 
     public static void main(String[] args) {
+        ArgumentParser parser = ArgumentParsers.newFor("prog").build()
+                .description("Process some integers.");
+        parser.addArgument("--width")
+                .dest("width")
+                .required(true)
+                .type(Integer.class)
+                .help("Width of the screen.");
+
+        Namespace ns = null;
+        try {
+            ns = parser.parseArgs(args);
+            ns.getInt("width");
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+
         PApplet.main(Main.class, args);
     }
 
